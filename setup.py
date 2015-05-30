d04f85b18dcc0419e32420a1c1e20a03fa4623ff
Philipp Eisenhauer <peisenha@Philipps-MacBook-Pro.local>
2015-05-30 17:10:46 -0500

    working towards pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'grmpy',
+  packages = ['grmpy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Estimator for the generalized Roy Model',
+  author = 'Philipp Eisenhauer',
+  author_email = 'eisenhauer@policy-lab.org',
+  url = 'https://github.com/grmToolbox/package', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['Generalized Roy', 'Policy Evaluation', 'Economics'], # arbitrary keywords
+  classifiers = [],
+)
