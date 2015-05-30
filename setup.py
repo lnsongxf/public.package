e3ed6d088f73498a1f7aa45c60f2e0e80ab858b0
Philipp Eisenhauer <peisenha@Philipps-MacBook-Pro.local>
2015-05-30 17:55:38 -0500

    fixing packaging issues

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
   author = 'Philipp Eisenhauer',
   author_email = 'eisenhauer@policy-lab.org',
   url = 'https://github.com/grmToolbox/package', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/grmToolbox/package/tarball/0.1', # I'll explain this in a second
   keywords = ['Generalized Roy', 'Policy Evaluation', 'Economics'], # arbitrary keywords
   classifiers = [],
+  install_requires=['coveralls','numdifftools','numpy','scipy','statsmodels','nose','pandas','matplotlib',],
 )
