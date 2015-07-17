83f0d4e566ab6b0e71ecde14c6ba2af74256a7c3
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-07-17 08:46:17 +0200

    all is well again

--- testing/random/investigate.py
+++ testing/random/investigate.py
@@ -16,16 +16,16 @@ from grmpy import *
 
 ''' Request
 '''
-label, seed = 'B', 185423
-#label, seed = 'C', 110836
+label, seed = 'C', 110836
 
 ''' Error Reproduction
 '''
 test = getattr(lib, 'test_' + label)
 
-
 np.random.seed(seed)
 
+# This is required to set the seeds identical to the
+# case in the run.py script.
 label = np.random.choice(['A', 'B', 'C', 'D', 'E'])
 
 test()
