1eaf6a12ca594aa7f404b2e3a530df3f9e678a7a
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-08-12 13:20:21 +0200

    putting in a bit of time on the example

--- example/auxiliary.py
+++ example/auxiliary.py
@@ -4,6 +4,40 @@
 # standard library
 import shlex
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def graph_marginal_effects(bmte, cmte, smte):
+    """ Plot the marginal effects of treatment in a single graphs.
+    """
+    # Initialize canvas
+    ax = plt.figure(figsize=(12,8)).add_subplot(111, axisbg='white')
+
+    # Plot marginal effects of treatment
+    x = np.linspace(0.01, 0.99, 99)
+    ax.plot(x, bmte, color='blue', linewidth=4, label=r''' $B^{MTE}$''',
+            linestyle='--')
+    ax.plot(x, cmte, color='darkgreen', linewidth=4, label=r''' $C^{MTE}$''',
+            linestyle='-.')
+    ax.plot(x, smte, color='red', linewidth=4, label=r''' $S^{MTE}$''',
+            linestyle=':')
+
+    # Set axis labels and ranges
+    ax.set_xlabel(r''' $u_S$''', fontsize=20)
+    ax.set_ylabel(''' Effect ''', fontsize=20)
+
+    # Set up legend
+    plt.legend(loc='upper center', bbox_to_anchor=(0.50, -0.10),
+               fancybox=False, frameon=False, shadow=False, ncol=3, fontsize=20)
+
+    # Remove first element on y-axis
+    ax.yaxis.get_major_ticks()[0].set_visible(False)
+
+    # Add title
+    plt.suptitle('Marginal Effects of Treatment', fontsize=20)
+
+    # Show plot
+    plt.show()
 
 def get_parameters():
 
